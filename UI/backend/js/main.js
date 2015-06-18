4f0a1f58361e562579fc1ad898b775bfa646c2b5
Session <sssession@homail.com>
2015-06-18 09:28:41 +0800

    framework

---
+++ UI/backend/js/main.js
@@ -0,0 +1,10 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery.min',
+        react: '../bower_components/react/react.min'
+    }
+});
+
+require([], function () {
+    return {};
+});
